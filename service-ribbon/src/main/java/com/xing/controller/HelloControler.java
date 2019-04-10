af535e68fb4e6e7357e1a894ee6ab491db71f5df
honxchen <honxchen@gmail.com>
2019-04-10 17:35:24 +0800

    init

---
+++ service-ribbon/src/main/java/com/xing/controller/HelloControler.java
@@ -0,0 +1,20 @@
+package com.xing.controller;
+
+import com.xing.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
